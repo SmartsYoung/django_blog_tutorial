7ba534d675c4ea78ba68daec374154bb193c01d1
SmartsYoung <yangcong_hit@163.com>
2021-02-27 15:50:49 +0800

    end of 34

--- comment/views.py
+++ comment/views.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.models import User
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from notifications.signals import notify
 
 from .forms import CommentForm
@@ -40,7 +40,7 @@ def post_comment(request, article_id, parent_comment_id=None):
                         action_object=new_comment,
                     )
 
-                return HttpResponse('200 OK')
+                return JsonResponse({"code": "200 OK", "new_comment_id": new_comment.id})
 
             new_comment.save()
 
@@ -53,7 +53,9 @@ def post_comment(request, article_id, parent_comment_id=None):
                     target=article,
                     action_object=new_comment,
                 )
-            return redirect(article)
+            # 添加锚点
+            redirect_url = article.get_absolute_url() + '#comment_elem_' + str(new_comment.id)
+            return redirect(redirect_url)
         else:
             return HttpResponse("表单内容有误，请重新填写。")
         # 处理 GET 请求
