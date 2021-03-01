f007ee8982d3eb729afd35aeb98646b42d594cf8
SmartsYoung <yangcong_hit@163.com>
2021-03-01 10:52:41 +0800

    end of 36

--- article/models.py
+++ article/models.py
@@ -103,3 +103,13 @@ class ArticlePost(models.Model):
             resized_image.save(self.avatar.path)
 
         return article
+
+
+    def was_created_recently(self):
+        diff = timezone.now() - self.created
+
+        # if diff.days <= 0 and diff.seconds < 60:
+        if diff.days == 0 and diff.seconds >= 0 and diff.seconds < 60:
+            return True
+        else:
+            return False
